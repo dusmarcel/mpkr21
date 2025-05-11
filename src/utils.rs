a352d7f7ed89b4e82e6b208a910d52376e952c56
marceldus <marcel@keienb.org>
2025-05-11 10:17:25 +0200

    switched from num-format to leptos-use

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,13 @@
-use num_format::{Locale, ToFormattedString};
+use leptos::prelude::*;
+use leptos_use::{use_intl_number_format, NumberStyle, UseIntlNumberFormatOptions};
 
 pub fn format_euro(value: f64) -> String {
-    let rounded = format!("{:.2}", value);
-    let parts: Vec<&str> = rounded.split('.').collect();
-    let int_part = parts[0].parse().unwrap_or(0);
-    let decimal_part = parts.get(1).unwrap_or(&"00");
-    let formatted_int = int_part.to_formatted_string(&Locale::de);
-    format!("{},{}", formatted_int, decimal_part)
+    use_intl_number_format(
+        UseIntlNumberFormatOptions::default()
+            .locale("de-DE")
+            .style(NumberStyle::Currency)
+            .currency("EUR")
+    )
+    .format::<f64>(value)
+    .get()
 }
